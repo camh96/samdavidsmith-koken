a894958694d7bb34176ece7fd8aca69f18d15fae
Cam Hovell <camhovell@gmail.com>
2016-03-15 03:27:49 -0400

    Initial commit

---
+++ storage/configuration/database.php
@@ -0,0 +1,9 @@
+<?php
+	return array(
+		'hostname' => 'localhost',
+		'database' => 'koken',
+		'username' => 'koken',
+		'password' => 'shei5lae4oHe',
+		'prefix' => 'koken_',
+		'socket' => ''
+	);
---
+++ storage/configuration/key.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Do not edit or remove this file unless advised by Koken support
+// support@koken.me
+
+return '128fc007a778134aaa675012761430c9';
